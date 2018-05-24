ed841999bd7868e76c8715dceb42c4dbdf10ef0e
Brandon Pate <bepate@att.net>
2018-05-24 21:20:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logShout(string) {
   }
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase()) {
+  if (string.toLowerCase()) {
     return "I can't hear you!";
   } if (string.ToLowerCase()) {
     return "YES INDEED!";
