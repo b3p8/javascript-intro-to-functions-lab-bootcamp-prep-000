883ceae0e99aa7d6e72f6feb98febd358fa191e7
Brandon Pate <bepate@att.net>
2018-05-24 21:08:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logShout(string) {
 function sayHiToGrandma(string) {
   if (string === lowercase) {
     return "I can't hear you!";
-  } else if (string === uppercase) {
+  } if (string === uppercase) {
     return "YES INDEED!";
-  } else {
+  } if (string === mixedcase) {
     return "I love you, Grandma."
   }
 }
